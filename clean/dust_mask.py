77697c528da0374e75d0748676bfa081ab7ab6ed
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2019-07-22 13:43:43 +0200

    Update galaxy map

--- clean/dust_mask.py
+++ clean/dust_mask.py
@@ -9,33 +9,45 @@ import matplotlib.pyplot as plt
 # Change this to find a suitable mask
 THRESHOLD = 0.005
 
+# Create plots directory
+if not os.path.exists("ris"):
+    print("Creating 'ris' directory...")
+    os.makedirs("ris")
+
 # Load the HFI 353 GHz map from the FITS file downloaded from the Planck Legacy Archive (PLA)
-hfi353 = hp.read_map("Milky_Way/HFI_SkyMap_353-psb_2048_R3.01_full.fits", verbose=False)
+print("Loading HFI 353 GHz map...")
+hfi353 = hp.read_map("../data/Milky_Way/HFI_SkyMap_353-psb_2048_R3.01_full.fits", verbose=False)
 
 # Reduce the resolution of the map in order to save memory and computational time
-hfi353 = hp.ud_grade(hfi353, 256)
+print("Changing resolution...")
+hfi353 = hp.ud_grade(hfi353, 1024)
 
 # Plot galaxy map without threshold - normalized as hist
-healpy.mollview(hfi353, coord='GE', norm="hist")
-plt.savefig('plots/elliptic_galaxy_map.png', dpi=600)
+print("Drawing elliptic map...")
+hp.mollview(hfi353, coord="GE", norm="hist")
+plt.savefig("ris/elliptic_galaxy_map.png", dpi=800)
 
-healpy.mollview(hfi353, norm="hist")
-plt.savefig('plots/galactic_galaxy_map.png', dpi=600)
+print("Drawing galactic map...")
+hp.mollview(hfi353, norm="hist")
+plt.savefig("ris/galactic_galaxy_map.png", dpi=800)
 
-#check if the dust mask already exists
-exists = os.path.isfile("HFI_dust_mask.fits.gz")
+
+# Check if the dust mask already exists
+exists = os.path.isfile("ris/HFI_dust_mask.fits.gz")
 if exists:
-    #if it exists, remove it
+    # If it exists, remove it
     print("-- Dust mask file already present. Il will be rewritten.")
-    os.remove("HFI_dust_mask.fits.gz")
+    os.remove("ris/HFI_dust_mask.fits.gz")
 
+
+print("Elaborating mask...")
 
 # Rotate the map from Galactic to Ecliptic coordinates
-rotator = healpy.rotator.Rotator(coord=['G','E'])
+rotator = hp.rotator.Rotator(coord=['G','E'])
 hfi353 = rotator.rotate_map_pixel(hfi353)
 
 # Apply a smoothing filter to the map
-hfi353 = healpy.smoothing(hfi353, fwhm=np.deg2rad(1.0), verbose=False)
+hfi353 = hp.smoothing(hfi353, fwhm=np.deg2rad(1.0), verbose=False)
 
 # Normalize the pixel values
 hfi353 -= np.min(hfi353)
@@ -46,12 +58,14 @@ hfi353[hfi353 <= THRESHOLD] = 0
 hfi353[hfi353 > THRESHOLD] = 1
 
 # Save the map in a new file
-healpy.write_map("HFI_dust_mask.fits.gz", hfi353, coord='E')
+print("Saving mask...")
+hp.write_map("ris/HFI_dust_mask.fits.gz", hfi353, coord='E')
 
-#save the map
-dust_map = healpy.read_map("HFI_dust_mask.fits.gz", verbose=False)
+# Load the map
+print("Loading mask...")
+dust_map = hp.read_map("ris/HFI_dust_mask.fits.gz", verbose=False)
 
-#save picture of the dust map
-healpy.mollview(dust_map)
-plt.show()
-plt.savefig('plots/eliptic_galaxy_mask.png', dpi=600)
+# Plot galaxy mask
+print("Drawing mask...")
+hp.mollview(dust_map)
+plt.savefig('ris/elliptic_galaxy_mask.png', dpi=800)
